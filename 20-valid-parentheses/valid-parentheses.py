4b0dbf5f04e48ad8180e396ef3d1b0cb98e49e6d
Eduardo Baccin <124003832+edu-baccin@users.noreply.github.com>
2024-12-02 16:34:56 -0300

    Time: 0 ms (100.00%) | Memory: 17.4 MB (6.41%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -3,21 +3,14 @@ from collections import deque
 
 class Solution:
     def isValid(self, s: str) -> bool:
-
-        left_side = ["(", "[", "{"]
-        right_side = [")", "]", "}"]
-        match = ["()", "[]", "{}"]
+        match = {")": "(", "]": "[", "}": "{"}
         stack = deque()
 
         for bracket in s:
-            if bracket in left_side:
+            if bracket in match.values():
                 stack.append(bracket)
-            elif len(stack) > 0 and stack[-1] + bracket in match:
-                stack.pop()
-            else:
-                return False
-
-        if len(stack) == 0:
-            return True
-        else:
-            return False
+            elif bracket in match.keys():
+                if not stack or match[bracket] != stack.pop():
+                    return False
+                
+        return not stack
