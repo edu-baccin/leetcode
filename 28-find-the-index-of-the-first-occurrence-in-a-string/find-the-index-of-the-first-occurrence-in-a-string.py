f07c7f7a35db33e6926f139c68ca4406cf85189c
Eduardo Baccin <124003832+edu-baccin@users.noreply.github.com>
2024-12-07 17:06:45 -0300

    Time: 0 ms (100.00%) | Memory: 17.1 MB (14.41%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,10 +1,3 @@
-import re
-
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        match = re.search(needle, haystack)
-
-        if match:
-            return match.start()
-        else:
-            return -1
+        return haystack.find(needle)
