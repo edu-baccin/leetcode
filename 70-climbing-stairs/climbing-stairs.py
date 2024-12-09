b2a243c274fd3b2aacf8e4410e4066e4b9f63cf2
Eduardo Baccin <124003832+edu-baccin@users.noreply.github.com>
2024-12-09 11:30:11 -0300

    Time: 0 ms (100.00%) | Memory: 17.1 MB (12.90%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -5,11 +5,11 @@ class Solution:
         elif n == 2:
             return 2
 
-        dp = [0] * (n+1)
-        dp[1] = 1
-        dp[2] = 2
+        result = [0] * (n+1)
+        result[1] = 1
+        result[2] = 2
         
         for i in range(3, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
+            result[i] = result[i-1] + result[i-2]
         
-        return dp[n]
+        return result[n]
